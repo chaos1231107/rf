c9c55df99bde8b64a01c402087e14ee53be39fcd
박건우 <136959177+chaos1231107@users.noreply.github.com>
2023-10-26 09:47:20 +0100

    Add files via upload

--- rf_ar.ino
+++ rf_ar.ino
@@ -1,16 +1,16 @@
 #include <SoftwareSerial.h>
 
-SoftwareSerial rfSerial(2, 3);  // YUL262RTX 모듈을 연결할 핀 번호
+SoftwareSerial rfSerial(2, 3);  
 
 void setup() {
-  rfSerial.begin(9600);  // YUL262RTX 모듈과의 시리얼 통신 초기화
-  Serial.begin(9600);     // 아두이노의 기본 시리얼 포트와의 통신 초기화
+  rfSerial.begin(9600);  
+  Serial.begin(9600);     
 }
 
 int number = 0;
 
 void loop() {
-  // 라즈베리 파이로 숫자 송신
+
   rfSerial.write(number);
 
   // 라즈베리 파이로부터 숫자 수신
@@ -21,11 +21,11 @@ void loop() {
     }
   }
 
-  // 숫자 출력 (아두이노의 시리얼 모니터에 출력)
+  
   Serial.print("Received number: ");
   Serial.println(number);
 
-  // 숫자 1 증가
+ 
   number++;
   delay(1000);
 }
