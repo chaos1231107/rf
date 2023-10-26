0a754c2ca572f5fa134cb6df25f7bd6a7ca2847d
박건우 <136959177+chaos1231107@users.noreply.github.com>
2023-10-26 09:48:32 +0100

    Add files via upload

---
+++ rf_check.py
@@ -0,0 +1,30 @@
+import serial
+import time
+
+ser = serial.Serial('/dev/ttyS0', 9600, timeout=1)
+
+number = 0
+
+while True:
+    try:
+        received_number = ser.readline().strip()
+        if received_number:
+            received_number = int(received_number)
+            if received_number > number:
+                number = received_number
+                
+        print(f"Received number: {number}")
+
+        number += 1
+        ser.write(f"{number}\n".encode())
+
+        #time.sleep(1)  
+    except KeyboardInterrupt:
+        break
+
+ser.close()
+
+
+
+			
+	
